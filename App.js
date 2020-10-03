c34b4a98c7b78447e3b754248a04d049181c5d90
Your Name <you@example.com>
2020-10-03 15:52:16 +0900

    update url

--- App.js
+++ App.js
@@ -3,6 +3,6 @@ import { WebView } from 'react-native-webview';
 
 export default class App extends React.Component {
   render() {
-    return <WebView source={{ uri: 'https://dawoori.github.io/silver-charge/' }} style={{ marginTop: 20 }} />;
+    return <WebView source={{ uri: 'https://smartsilver.netlify.app/' }} style={{ marginTop: 20 }} />;
   }
 }
